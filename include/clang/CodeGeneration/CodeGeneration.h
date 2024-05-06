5336eb1927441348e9978e7497687f6dae265255
McCorney Severin <severin@severinmbpro.lan>
2024-05-06 12:30:49 -0700

    Updating CodeGenration

---
+++ include/clang/CodeGeneration/CodeGeneration.h
@@ -0,0 +1,14 @@
+#ifndef LLVM_CLANG_CODEGENERATION_CODEGENERATION_H
+#define LLVM_CLANG_CODEGENERATION_CODEGENERATION_H
+
+#include <memory>
+
+namespace clang {
+
+namespace codegen {
+
+}
+
+}
+
+#endif 
