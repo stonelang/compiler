9646e04778154561415de012a33cf74cf96dc703
McCorney Severin <severin@severinmbpro.lan>
2024-05-06 18:16:01 -0700

    Updating Decl

--- include/clang/Syntax/Decl.h
+++ include/clang/Syntax/Decl.h
@@ -3,6 +3,7 @@
 
 #include "clang/Core/InlineBitfield.h"
 #include "clang/Syntax/ASTAllocation.h"
+#include "clang/Syntax/TypeAlignment.h"
 
 #include "llvm/ADT/ArrayRef.h"
 
@@ -19,12 +20,64 @@ enum class DeclKind : uint8_t {
 #include "clang/Syntax/DeclNode.inc"
 };
 
- enum : unsigned { NumDeclKindBits =
-   clang::CountBitsUsed(static_cast<unsigned>(DeclKind::LastDecl)) };
-
-class Decl : public ASTAllocation<Decl> {
-public:
+enum : unsigned {
+  NumDeclKindBits =
+      clang::CountBitsUsed(static_cast<unsigned>(DeclKind::LastDecl))
 };
+
+class alignas(1 << DeclAlignInBits) Decl : public ASTAllocation<Decl> {
+protected:
+  DeclKind kind;
+
+protected:
+  union {
+    uint64_t OpaqueBits;
+    CLANG_INLINE_BITFIELD_BASE(
+        Decl, clang::BitMax(NumDeclKindBits, 8) + 1 + 1 + 1 + 1 + 1 + 1, Kind
+        : clang::BitMax(NumDeclKindBits, 8),
+
+          /// Whether this declaration is invalid.
+          IsValid : 1,
+
+          /// Whether this declaration was implicitly created, e.g.,
+          /// an implicit constructor in a struct.
+          IsImplicit : 1,
+
+          /// Whether this declaration was mapped directly from a Clang AST.
+          ///
+          /// Use GetClangNode() to retrieve the corresponding Clang AST.
+          IsFromClang : 1,
+
+          /// Whether this declaration was added to the surrounding
+          /// DeclContext of an active #if config clause.
+          Refere : 1,
+
+          /// Whether this declaration is syntactically scoped inside of
+          /// a local context, but should behave like a top-level
+          /// declaration for name lookup purposes. This is used by
+          /// lldb.
+          Used : 1,
+
+          /// Wether this is a top level decl
+          IsTopLevel : 1);
+  } Bits;
+
+public:
+  Decl() = delete;
+  Decl(const Decl &) = delete;
+  Decl(Decl &&) = delete;
+  Decl &operator=(const Decl &) = delete;
+  Decl &operator=(Decl &&) = delete;
+
+public:
+  Decl(DeclKind kind) : kind(kind) {}
+};
+
+class ImportDecl : public Decl {
+public:
+  ImportDecl() : Decl(DeclKind::Import) {}
+};
+
 } // namespace syntax
 
 } // end namespace clang
---
+++ include/clang/Syntax/TypeAlignment.h
@@ -0,0 +1,13 @@
+#ifndef LLVM_CLANG_SYNTAX_TYPEALIGNMENT_H
+#define LLVM_CLANG_SYNTAX_TYPEALIGNMENT_H
+
+#include "llvm/ADT/ArrayRef.h"
+#include <memory>
+
+namespace clang {
+namespace syntax {
+constexpr size_t DeclAlignInBits = 8;
+}
+} // namespace clang
+
+#endif
